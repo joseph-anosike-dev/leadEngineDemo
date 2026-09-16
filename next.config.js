08d8d6ee883ec0cf75a223194db8fa062158e956
Joseph Anosike <joseph.anosike.github@gmail.com>
2026-09-16 01:51:05 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,6 +9,12 @@ const nextConfig = {
         hostname: "*.supabase.co",
         pathname: "/storage/v1/object/public/**",
       },
+
+      {
+        protocol: "https",
+        hostname: "picsum.photos"
+
+      },
     ],
   },
 };
